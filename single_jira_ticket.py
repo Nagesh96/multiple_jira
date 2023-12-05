efe1db1d244e8efd589995d3e20cc2e964274e00
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-05 19:40:29 +0530

    Update single_jira_ticket.py

--- single_jira_ticket.py
+++ single_jira_ticket.py
@@ -1,26 +1,28 @@
 from jira import JIRA
 import sys
 
-def update_issue_field(username, password, issue_key, field_id, new_value):
+def update_issue_field(username, password, issue_keys, field_id, new_value):
     jira_url = "https://jira.charter.com"
 
     try:
         jira = JIRA(server=jira_url, basic_auth=(username, password))
-        issue = jira.issue(issue_key)
 
-        # Prepare the update field dictionary
-        update_fields = {field_id: new_value}
+        for issue_key in issue_keys:
+            issue = jira.issue(issue_key)
 
-        issue.update(fields=update_fields)
+            # Prepare the update field dictionary
+            update_fields = {field_id: new_value}
 
-        print(f"Field '{field_id}' in issue '{issue_key}' updated successfully to '{new_value}'.")
+            issue.update(fields=update_fields)
+
+            print(f"Field '{field_id}' in issue '{issue_key}' updated successfully to '{new_value}'.")
     except Exception as e:
         print(f"Failed to update the field '{field_id}'. Error:", str(e))
 
-# Set your Jira username, password, and issue key here
+# Set your Jira username, password, and issue keys here
 username = "P3214461"
 password = "Home16Lander*"
-jiraTicket = "MOBIT2-31903"
+jiraTickets = ["MOBIT2-31903", "MOBIT2-31904"]  # Example list of Jira ticket IDs
 
 # Check if correct number of arguments is provided
 if len(sys.argv) != 2:
@@ -31,4 +33,4 @@ if len(sys.argv) != 2:
 field_arg = sys.argv[1]
 field_id, new_value = field_arg.split('=')
 
-update_issue_field(username, password, jiraTicket, field_id, new_value)
+update_issue_field(username, password, jiraTickets, field_id, new_value)
