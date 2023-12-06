891cca5b2a9071c399b8f7455009ae6df8cfebe0
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-06 19:23:34 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,1 +1,4 @@
 https://jfrog.charter.com/artifactory/mobile-snapshot-local/com/spectrum/mobile/mbo-dataassetbuildup/4.0.0-SNAPSHOT/mbo-dataassetbuildup-4.0.0-SNAPSHOT.war
+
+
+  https:\/\/jfrog\.sample\.com\/artifactory\/[\w-]+\/[\w.-]+\/[\w.-]+\/[\d.]+-(SNAPSHOT|\d+\.\d+\.\d+)\/[\w.-]+-[\d.]+-(SNAPSHOT|\d+\.\d+\.\d+)\.(war|jar)
