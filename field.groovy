055e2f262409384a6686977d7f0bc1f35cf7bde3
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-06 17:47:56 +0530

    Update field.groovy

--- field.groovy
+++ field.groovy
@@ -1,33 +1,66 @@
-stage('Upload artifacts to JFrog Repo') {
-    steps {
-        script {
-            def server = Artifactory.server('Artifactory')
-            def rtMaven = Artifactory.newMavenBuild()
-            
-            def uploadSpec = [
-                files: [
-                    [
-                        pattern: '*.war',
-                        target: 'example-repo-local'
-                    ]
-                ]
-            ]
-            
-            // Capture console output in a variable
-            def consoleOutput = server.upload(uploadSpec).trim()
-            
-            // Extracting JFrog URL using regex
-            def jfrogUrlRegex = ~/https?:\/\/(?:www\.)?jfrog\.com\/[^\s]+/
-            def jfrogUrlMatch = (consoleOutput =~ jfrogUrlRegex)
-            
-            if (jfrogUrlMatch.find()) {
-                def jfrogUrl = jfrogUrlMatch[0]
-                echo "JFrog URL: ${jfrogUrl}"
-                // Use jfrogUrl variable as needed
-            } else {
-                echo "JFrog URL not found in console output"
-                // Handle the case when the URL is not found
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'JIRA_TICKETS', defaultValue: '', description: 'Provide the Jira ticket no')
+    }
+
+    tools {
+        maven 'maven-3'
+    }
+
+    stages {
+        stage('Validate Inputs') {
+            steps {
+                script {
+                    echo "USER INPUT ........"
+                    //env.JIRA_TICKETS = ${params.JIRA_TICKETS}
+                    echo "JiraTickets: ${JIRA_TICKETS}"
+                }
             }
         }
+        
+        stage('git checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'git-cred', url: 'https://github.com/Nagesh96/addressbook.git']])
+            }
+        }
+        
+        stage('Compile and Build') {
+            steps {
+                echo 'building the artifacts'
+                sh "mvn clean install -U -DskipTests=true"
+            }
+        }
+
+        stage('Upload artifacts to JFrog Repo') {
+            steps {
+                echo 'Publishing Artifacts'
+                script {
+                    def server = Artifactory.server('Artifactory')
+                    def rtMaven = Artifactory.newMavenBuild()
+                    def uploadSpec = """
+                        {
+                            "files": [{
+                                "pattern": "*.war",
+                                "target": "example-repo-local"
+                            }]
+                        }"""
+                    
+                    def buildInfo = server.upload(uploadSpec)
+                    server.publishBuildInfo(buildInfo)
+                    echo "Artifact Display Url: ${env.RUN_ARTIFACTS_DISPLAY_URL}"
+                }
+            }
+        }
+        
+        /*stage('Run Python') {
+            steps {
+                script {
+                    sh "pwd"
+                    sh "python3 field.py"
+                }
+            }
+        }*/
     }
 }
