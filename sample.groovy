3a3b331bcb08e86964d0d64f3fa087f70843b6fb
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-06 20:01:17 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -2,3 +2,28 @@ https://jfrog.charter.com/artifactory/mobile-snapshot-local/com/spectrum/mobile/
 
 
   https:\/\/jfrog\.sample\.com\/artifactory\/[\w-]+\/[\w.-]+\/[\w.-]+\/[\d.]+-(SNAPSHOT|\d+\.\d+\.\d+)\/[\w.-]+-[\d.]+-(SNAPSHOT|\d+\.\d+\.\d+)\.(war|jar)
+
+
+
+stage('Upload artifacts to JFrog Repo') {
+    steps {
+        echo 'Publishing Artifacts'
+        script {
+            def server = Artifactory.server('Artifactory')
+            def rtMaven = Artifactory.newMavenBuild()
+            def uploadSpec = """
+                {
+                    "files": [{
+                        "pattern": "*.war",
+                        "target": "example-repo-local"
+                    }]
+                }"""
+            
+            def buildInfo = server.upload(uploadSpec)
+            server.publishBuildInfo(buildInfo)
+          
+            env.ARTIFACT_URL = server.getUrl() + "/example-repo-local/your-artifact.war"
+            echo "Artifact Display Url: ${env.ARTIFACT_URL}"
+        }
+    }
+}
