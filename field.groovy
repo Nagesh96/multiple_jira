20a181271bcc1695e9f3b2eb8d8944938213dc2c
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-06 13:14:37 +0530

    Update field.groovy

--- field.groovy
+++ field.groovy
@@ -1,7 +1,25 @@
-// Capture and store the uploaded artifact's URL
+stage('Upload artifacts to JFrog Repo') {
+    steps {
+        echo 'Publishing Artifacts'
+        script {
+            def server = Artifactory.server('Artifactory')
+            def rtMaven = Artifactory.newMavenBuild()
+            def uploadSpec = """
+                {
+                    "files": [{
+                        "pattern": "*.war",
+                        "target": "example-repo-local"
+                    }]
+                }"""
+            
+            def buildInfo = server.upload(uploadSpec)
+            server.publishBuildInfo(buildInfo)
+            
             def uploadedArtifact = buildInfo.getDeployableArtifacts().get(0)
             def artifactUrl = uploadedArtifact.getDeployableArtifact().getRemotePath()
-            env.ARTIFACTORY_URL = "${server.getUrl()}/${artifactUrl}"
+            env.ARTIFACTORY_URL = "${server.getUrl()}${artifactUrl}"
+            
+            echo "Artifact URL: ${env.ARTIFACTORY_URL}"
         }
     }
 }
